43ec2cce9f63b99f23bbe94d7d3e568d965096fc
github username <github email address>
2019-01-29 09:43:58 +0000

    learn submit

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,46 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+  'ArrowUp',
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+  'ArrowLeft',
+  'ArrowRight',
+  'b',
+  'a'
+]
 
-function init() {
+let newA = []
+let index = 0
+
+function init () {
   // your code here
+
+  document.body.addEventListener('keydown', e => {
+    if (e.key === codes[index]) {
+      // console.log(e.key)
+      // console.log(codes[index])
+      newA.push(e.key)
+
+      index++
+    } else {
+      // console.log(e.key)
+      // console.log(codes[index])
+      // console.log(index)
+      // console.log('restart')
+      index = 0
+      // console.log(index)
+      newA = []
+    }
+
+    if (index === 10) {
+      alert("Congrats!")
+    }
+  })
+
 }
+
+
+
+
