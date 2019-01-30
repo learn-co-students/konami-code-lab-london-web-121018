54d3d3d178403f55def95236fb84f7a9706ab877
timtan93 <timtan93@gmail.com>
2019-01-30 16:14:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
+const input = document.querySelector('body');
+let index = 0
 function init() {
   // your code here
+  input.addEventListener('keydown', function(e) {
+  const key = e.key;
+
+
+
+  if (key === codes[index]) {
+    index++;
+
+    if (index === codes.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+
+});
 }
