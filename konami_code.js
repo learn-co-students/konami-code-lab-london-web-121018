54afa06bb24aa0da7c5dbe8c4cd1291991c33d7d
georgejeffery <georgejeffery@gmail.com>
2019-01-31 14:49:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,27 @@ const codes = [
 
 function init() {
   // your code here
+  let codeIndex = 0
+
+  function onKeyDown(key) {
+
+    const currentKey = key.key
+
+
+    if (currentKey === codes[codeIndex]) {
+      codeIndex++;
+      console.log(key)
+
+      if (codeIndex === codes.length) {
+        window.alert("Correct!");
+        console.log("win!")
+        codeIndex = 0;
+      }
+    } else {
+      codeIndex = 0;
+    }
+  }
+
+  document.body.addEventListener('keydown', onKeyDown)
+
 }
