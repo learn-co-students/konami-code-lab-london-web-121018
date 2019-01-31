fd4a5269f03106fa0b5fee68fc16e18dc73a6325
Dark-Raine <anderson-tomas@hotmail.com>
2019-01-31 12:26:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
+  let indx = 0;
+  document.body.addEventListener('keydown', (letter) => {
+    let comp = letter.key;
+  
+    if (comp === codes[indx]) {
+      ++indx
+      if (indx === codes.length) {
+        alert('Correct!')
+      }
+    } else {
+      indx = 0;
+    }
+  })
+
+
 }
