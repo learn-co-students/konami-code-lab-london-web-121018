02b0dcb96b837d6f92ef16072939aceeab509141
github username <github email address>
2019-02-19 16:07:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,21 @@ const codes = [
 ];
 
 function init() {
+
+  let codeIndex = 0;
   // your code here
-}
+  document.body.addEventListener('keydown', function (e) {
+    const key = e.key;
+
+    if (codes[codeIndex] === key) {
+      codeIndex += 1;
+    } else {
+      codeIndex = 0;
+    };
+
+    if (codeIndex === codes.length) {
+      window.alert("You entered the secret code 👀");
+      idx = 0;
+    };
+  });
+};
